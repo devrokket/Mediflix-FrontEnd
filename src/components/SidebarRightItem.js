87e1b9b276dd303851f881743ce01a92d1bf2977
devrokket <rokany@cau.ac.kr>
2023-03-20 00:42:33 +0900

    test file

---
+++ src/components/SidebarRightItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/sidebar-right.css";
+
+function SidebarRightItem({ menu, extra }) {
+  return (
+    <div className="sidebar-right-item">
+      <p>{menu.name}</p>
+       {extra && <div className="sidebar-right-item-extra">{extra}</div>}
+    </div>
+  );
+}
+
+export default SidebarRightItem;
---
+++ src/pages/PartnerMange.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PartnerMange() {
+  return (
+    <div>
+      <h2>PartnerMange</h2>
+      <p>This is PartnerMange screen!</p>
+    </div>
+  );
+}
+
+export default PartnerMange;
