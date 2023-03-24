93d72d5db74dc7a68e9afa773329d76052a5e74c
devrokket <rokany@cau.ac.kr>
2023-03-24 19:36:33 +0900

    detail page img

---
+++ src/components/SidebarLeftItem.js
@@ -0,0 +1,15 @@
+// import React from "react";
+// import "../assets/styles/sidebar-left-item.css";
+
+// function SidebarLeftItem(props) {
+//   const { menu, isActive } = props;
+
+//   return (
+//     <div className={`sidebar-left-item ${isActive ? "active" : ""}`}>
+//       <img src={menu.image} alt={menu.name} />
+//       <span style={{ 'marginLeft': 15 }}>{menu.name}</span>
+//     </div >
+//   );
+// }
+
+// export default SidebarLeftItem;
